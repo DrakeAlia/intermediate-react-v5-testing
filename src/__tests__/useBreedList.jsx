0630e4377b81ea1a99f0532f40d510a7596cb72e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-02-26 09:30:14 -0800

    testing mocks

--- src/__tests__/useBreedList.jsx
+++ src/__tests__/useBreedList.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 
@@ -41,3 +41,46 @@ test("gives an emnpty list with no animal", async () => {
     expect(breedList).toHaveLength(0);
     expect(status).toBe("loading");
 });
+
+// Let's write a second test for actually making a request with our custom hook, useBreedList. 
+// But we have a problem: we don't actually want to fetch from our API.This can be slow and cause 
+// unnecessary load on a server or unnecessary complexity of spinning up a testing API.We can instead mock the call.
+// A mock is a fake implementation.We could write our own fake fetch but a good one already exists 
+// for Vitest called vitest - fetch - mock so let's install that
+
+test("gives back breeds when given an animal", async () => {
+    const breeds = [
+        "Havanese",
+        "Bichon Frise",
+        "Poodle",
+        "Maltese",
+        "Golden Retriever",
+        "Labrador",
+        "Husky",
+    ];
+    // We're using the fetch.mockResponseOnce function to mock the fetch function. We're passing in
+    // the JSON.stringify function to convert the breeds array into a JSON string.
+    fetch.mockResponseOnce(
+        JSON.stringify({
+            animal: "dog",
+            breeds,
+        })
+    )
+    const { result } = renderHook(() => useBreedList("dog"), {
+        wrapper: ({ children }) => (
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        ),
+    })
+    // We're using the waitFor function from the @testing-library/react package to wait for the
+    // status to be "success".
+    // In general you should mock API calls. It will make tests run much faster and 
+    // save unnecessary load on an API. 
+    await waitFor(() => expect(result.current[1]).toBe("success"));
+    // We're using the destructuring assignment to get the breedList variable from the result object.
+    const [breedList] = result.current;
+    // We're also using the toEqual matcher to make sure that the breedList is equal to the breeds
+    // array.
+    expect(breedList).toEqual(breeds);
+})
